a80dc38e92528534c952fc8b22852073339fdcee
Yuanyi Wu <wuyuanyi135@gmail.com>
2019-09-16 10:11:11 -0400

    cross platform proto compilation

--- compile_proto.py
+++ compile_proto.py
@@ -2,18 +2,18 @@ import subprocess as sp
 import os.path as osp
 import os
 import sys
+import sys
 
 if __name__ == '__main__':
     current_dir = osp.dirname(__file__)
     sp.call([
-        f"{osp.join(sys.prefix,'bin/')}python",
+        f"{osp.join(sys.prefix, 'bin/' if sys.platform == 'linux' else 'Scripts/')}python",
         "-m",
         "grpc_tools.protoc",
         f"--python_out={current_dir}",
         f"--grpc_python_out={current_dir}",
         f"--mypy_out={current_dir}",
-        f"--plugin=protoc-gen-mypy={osp.join(sys.prefix, 'bin', 'protoc-gen-mypy')}",
+        f"--plugin=protoc-gen-mypy={osp.join(sys.prefix, 'bin' if sys.platform == 'linux' else 'Scripts/', 'protoc-gen-mypy' if sys.platform == 'linux' else 'protoc_gen_mypy.bat')}",
         f"-I.",
-        "./inference_service.proto"
+        "inference_service.proto"
     ])
-
